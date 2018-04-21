10cb234c41c23ee556096125497c110531215f92
hkdnet <satoko.itse@gmail.com>
2018-04-22 01:28:12 +0900

    return dummy response

--- http.scm
+++ http.scm
@@ -1,15 +1,28 @@
 (use gauche.net)
 
+(define (parse-header header)
+  (let ((lines (string-split header "\r\n")))
+    (fold cons '() lines)))
+
+(define (parse-request request)
+  (let ((arr (string-split request "\r\n\r\n" 2)))
+    (let ((header (car arr)) (body (cdr arr)))
+      (parse-header header))))
+
+(define (build-response headers body)
+  "HTTP/1.1 200 OK\r\nContent-Length: 7\r\n\r\nHello\r\n")
+
 (define (handler sock)
   (let ((recv (socket-recv sock 1024)))
-    (display "written!\n")
     (if (<= (string-length recv) 0)
       (begin
         (display "exit\n")
         (socket-close sock)
         (exit)))
-    (display recv)(newline)
-    (socket-send sock recv))
+    (let ((parsed (parse-request recv)))
+      (let ((headers (car parsed)) (body (cdr parsed)))
+        (let ((resp (build-response headers body)))
+          (socket-send sock resp)))))
   (handler sock))
 
 (define (main args)
