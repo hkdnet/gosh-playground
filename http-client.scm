b39f6cd29d516441b529d8bb5567e0faee33eed7
hkdnet <satoko.itse@gmail.com>
2018-04-21 20:15:29 +0900

    ファイルから読んだ文字列を流すようにした

--- http-client.scm
+++ http-client.scm
@@ -1,5 +1,17 @@
 (use gauche.net)
 
+(define (read-from-file filename)
+  (let ((p (open-input-file filename)))
+    (let loop ((ls1 '()) (c (read-char p)))
+      (if (eof-object? c)
+        (begin
+          (close-input-port p)
+          (let ((str (list->string (reverse ls1))))
+            str))
+        (loop (cons c ls1) (read-char p)))))
+  )
+
 (define (main args)
-  (let ((client-sock (make-client-socket "127.0.0.1" 5000)))
-    (socket-send client-sock "Hello\n")))
+  (receive (_ filename) (apply values args)
+    (let ((client-sock (make-client-socket "127.0.0.1" 5000)))
+      (socket-send client-sock (read-from-file filename)))))
