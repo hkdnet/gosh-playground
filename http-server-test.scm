a44c55c12a19967acbc34896e553a5d67c990c70
hkdnet <satoko.itse@gmail.com>
2018-04-22 03:15:28 +0900

    [cosme]

--- http-server-test.scm
+++ http-server-test.scm
@@ -4,6 +4,9 @@
 (load "./http-server")
 
 (test-section "parse-first-line")
-(test "feature 1-1" `((method ,"GET") (path ,"/")) (lambda () (parse-first-line "GET / HTTP/1.1" )))
+(test
+  "feature 1-1"
+  '((method "GET") (path "/"))
+  (lambda () (parse-first-line "GET / HTTP/1.1\n" )))
 
 (test-end :exit-on-failure #t)
