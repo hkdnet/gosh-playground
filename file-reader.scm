40f6b94b43807626d5befbb224d38abe53f6ce7b
hkdnet <satoko.itse@gmail.com>
2018-04-26 00:07:00 +0900

    Remove an unnecessary file

--- file-reader.scm
+++
@@ -1,8 +0,0 @@
-(define (read-from-file filename)
-  (with-input-from-file filename
-    (lambda ()
-      (let loop ((ls1 '()) (c (read-char)))
-        (if (eof-object? c)
-          (let ((str (list->string (reverse ls1))))
-            str)
-          (loop (cons c ls1) (read-char)))))))
