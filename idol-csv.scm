2030322da233f7cfb804fb8ff86586ad3f41d0c5
hkdnet <satoko.itse@gmail.com>
2018-04-30 22:38:42 +0900

    tmp

--- idol-csv.scm
+++ idol-csv.scm
@@ -11,9 +11,18 @@
 
 (define-record-type idol #t #t header)
 
-(let loop ([bs bodies])
-  (if (not (null? bs))
-    (let ((b (car bs)))
-      (let ((idol (make-idol b)))
-        (display idol)(newline)
-        (loop (cdr bs))))))
+(define (bodies->idols bodies)
+  (let loop ([bs bodies] [idols '()])
+    (if (null? bs)
+      idols
+      (let ((b (car bs)))
+        (let ((idol (make-idol b)))
+          (loop (cdr bs) (cons idol idols)))))))
+
+(define idols (bodies->idols bodies))
+
+(let loop ([idols idols])
+  (if (not (null? idols))
+    (let ((idol (car idols)))
+      (display idol)(newline)
+      (loop (cdr idols)))))
