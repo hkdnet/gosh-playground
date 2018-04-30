932704ac4bdda2aee927a5145e68fdd0408aecde
hkdnet <satoko.itse@gmail.com>
2018-04-30 17:01:42 +0900

    cosme

--- csv-test.scm
+++ csv-test.scm
@@ -3,6 +3,7 @@
 (with-module CSV
   (use gauche.test)
   (test-start "csv")
+
   (test-section "to-lines")
   (test "with \\n" '("a" "b") (lambda() (to-lines "a\nb")))
   (test "with \\r\\n" '("a" "b") (lambda() (to-lines "a\r\nb")))
