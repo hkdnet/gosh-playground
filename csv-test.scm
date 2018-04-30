2b545dd2c7b617d03977c1ca39c83b7881138388
hkdnet <satoko.itse@gmail.com>
2018-04-30 17:19:56 +0900

    file->csv

--- csv-test.scm
+++ csv-test.scm
@@ -7,4 +7,7 @@
   (test-section "parse-csv")
   (test "with \\n" '(("a" "b") ("c" "d")) (lambda() (parse-csv "a,b\nc,d")))
   (test "with \\n" '(("a" "b") ("c" "d")) (lambda() (parse-csv "a,b\r\nc,d")))
+
+  (test-section "file->csv")
+  (test "csv/idols.csv" '(("name" "age") ("Miki" "15") ("Ritsuko" "19")) (lambda() (file->csv "csv/idols.csv")))
 )
--- csv.scm
+++ csv.scm
@@ -1,11 +1,13 @@
 (define-module CSV
 
 (use file.util)
+(use srfi-13)
+
 ; TODO: " で囲われてるときとかがあるので結局自前で1文字ずつ見るしかない
 (define (to-lines text)
   (string-split text #/\r?\n/))
 (define (parse-csv text)
-  (let ((lines (to-lines text)))
+  (let ((lines (filter (lambda (line) (not (string-null? line))) (to-lines text))))
     (map (lambda (line) (string-split line ",")) lines)))
 (define (file->csv filename)
   (let ((text (file->string filename)))
